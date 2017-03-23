b57bf9a8716ff9808a83d683f3071f17d9932781
Joseph <josephcaillet@gmail.com>
2017-03-23 12:12:19 +0100

    added header file with Letter and animation, with demo code.

---
+++ header.c
@@ -0,0 +1,49 @@
+//Representation d'une lettre
+typedef enum
+{
+	____ = 0,
+	___A = 1,
+	__L_ = 2,
+	__LA = 3,
+	_A__ = 4,
+	_A_A = 5,
+	_AL_ = 6,
+	_ALA = 7,
+	G___ = 8,
+	G__A = 9,
+	G_L_ = 10,
+	G_LA = 11,
+	GA__ = 12,
+	GA_A = 13,
+	GAL_ = 14,
+	GALA = 15,
+	END
+} Lettre;
+
+//Représentation d'une animation
+typedef struct
+{
+	Lettre* steps;
+	int pause;
+	int iterations;
+} Animation;
+
+#include <stdio.h>
+void main()
+{
+	Lettre l = GALA;
+	printf("%d", l);
+	Animation a = {.steps = (Lettre[]){G___, GA__, GAL_, GALA, END},
+					.pause = 100,
+					.iterations = 20};
+	int i = 0;
+	while(1)
+	{
+		if(a.steps[i] == END)
+		{
+			break;
+		}
+		printf("%d\t%d\t%d\n", a.steps[i], a.pause, a.iterations);
+		i++;
+	}
+}
